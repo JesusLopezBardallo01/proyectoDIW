e388fcc3ddc38c44868cc02b310448b506987a7c
JesusLopezBardallo01 <74413761+JesusLopezBardallo01@users.noreply.github.com>
2023-03-02 16:43:36 +0100

    Controlar si la fecha no está seleccionada

--- php/registropedido.php
+++ php/registropedido.php
@@ -15,15 +15,24 @@
     $user = "root";
     $pass = "";
     $conexion = mysqli_connect($nombre, $user, $pass, $bd);
-    //$fechaAlta= date('Y-m-d');
     $fechainicio = $_POST["fechainicio"];
     $fechafin = $_POST["fechafin"];
     $precio = $_POST["precio"];
 
-    $query = "INSERT INTO mazos_alquilados(fecha_inicio,fecha_fin,precio) values ('$fechainicio','$fechafin','$precio')";
+    // Consulta para verificar si las fechas ya están en uso
+    $query = "SELECT * FROM mazos_alquilados WHERE ((fecha_inicio >= '$fechainicio' AND fecha_inicio <= '$fechafin') OR (fecha_fin >= '$fechainicio' AND fecha_fin <= '$fechafin') OR ('$fechainicio' >= fecha_inicio AND '$fechainicio' <= fecha_fin) OR ('$fechafin' >= fecha_inicio AND '$fechafin' <= fecha_fin))";
     $registro = mysqli_query($conexion, $query);
-    echo ("El registro del pedido se ha realizado correctamente");
-    header("Location:" . "../vista/index.html");
+    if (mysqli_num_rows($registro) > 0) {
+        // Si hay resultados, las fechas ya están en uso, se muestra la página de error
+        header("Location: ../vista/erroralquiler.html");
+        exit();
+    } else {
+        // Si no hay resultados, se realiza la inserción
+        $query = "INSERT INTO mazos_alquilados(fecha_inicio,fecha_fin,precio) values ('$fechainicio','$fechafin','$precio')";
+        $registro = mysqli_query($conexion, $query);
+        echo ("El registro del pedido se ha realizado correctamente");
+        header("Location:" . "../vista/index.html");
+    }
     ?>
 </body>
 
